c1c5743db0636e046c3f6d37f2078b63542d2f48
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-06-23 21:25:22 +0300

    add

--- src/components/ExerciseVideos.jsx
+++ src/components/ExerciseVideos.jsx
@@ -5,33 +5,36 @@ const ExerciseVideos = ({ exerciseVideos, name }) => {
   if (!exerciseVideos.length) return 'Loading...';
   return (
     <Box sx={{ marginTop: { lg: '200px', xs: '20px' } }} p='20px'>
-      <Typography
-        sx={{ fontSize: { lg: '44px', xs: '25px' } }}
-        fontWeight={700}
-        color='#000'
-        mb='33px'
-      >
+      <Typography variant='h3' mb='33px'>
         Watch{' '}
-        <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>
+        <span style={{ color: '#ff2625', textTransform: 'capitalize' }}>
           {name}
         </span>{' '}
         exercise videos
       </Typography>
       <Stack
-        sx={{ flexDirection: { lg: 'row' }, gap: { lg: '110px', xs: '0px' } }}
         justifyContent='flex-start'
         flexWrap='wrap'
         alignItems='center'
+        sx={{ flexDirection: { lg: 'row' }, gap: { lg: '80px', xs: '0' } }}
       >
-        {exerciseVideos?.slice(0, 6)?.map((item, index) => (
+        {exerciseVideos?.slice(0, 6).map((item, index) => (
           <a
+            href={`https://youtube.com/watch?v=${item.video.videoId}`}
             key={index}
             className='exercise-video'
-            href={`https://www.youtube.com/watch?v=${item.video.videoId}`}
             target='_blank'
             rel='noreferrer'
           >
             <img src={item.video.thumbnails[0].url} alt={item.video.title} />
+            <Box>
+              <Typography variant='h5' color='#000'>
+                {item.video.title}
+              </Typography>
+              <Typography variant='h6' color='#494646'>
+                {item.video.channelName}
+              </Typography>
+            </Box>
           </a>
         ))}
       </Stack>
--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -30,7 +30,7 @@ const ExerciseDetail = () => {
         `${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`,
         youtubeOptions
       );
-      setExerciseVideos(exerciseVideosData);
+      setExerciseVideos(exerciseVideosData.contents);
     };
 
     fetchExercisesData();
